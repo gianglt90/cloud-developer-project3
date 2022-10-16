80db2ae46c07cb850c7577c4c87bec4bd2ea076b
Bale Le <gianglt90@gmail.com>
2022-10-16 14:45:52 -0400

    Add k8s yaml file

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a59425a0d137c49b79ff10224e8501f6-879478157.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
